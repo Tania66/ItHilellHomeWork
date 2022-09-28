12d79b70ba59425bfc3ee348cc240774d3f6330a
Your Name <you@example.com>
2022-09-28 23:14:59 +0300

    HW12: Lottery

---
+++ src/it/hillel/hw12/HW12Main.java
@@ -0,0 +1,41 @@
+package it.hillel.hw12;
+
+import java.util.Arrays;
+import java.util.Random;
+public class HW12Main {
+    public static void main(String[] args) {
+
+
+        Random random = new Random();
+
+        int [] company = new int[7];
+
+        for (int i =0; i<company.length;i++) {
+            company[i] = random.nextInt(10);
+        }
+        Arrays.sort(company);
+        System.out.println("Write number 0 to 9");
+
+
+
+        int [] player = new int[7];
+        for (int i = 0; i<player.length;i++){
+            player[i] = random.nextInt(10);
+            Arrays.sort(player);
+        }
+        int count = 0;
+        for (int i = 0; i < 7; i++) {
+            if (company[i]==player[i])count++;
+        }
+
+        System.out.println( Arrays.toString(player));
+        System.out.println(" ");
+        System.out.println( Arrays.toString(company));
+        System.out.printf("Number of matches: " + count);
+    }
+}
+
+
+
+
+
